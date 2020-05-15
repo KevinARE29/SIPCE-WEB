c9c130582e8b85f8b5039d2fb44c0d59d29acac1
Esmeralda López <lorenaeguadron@gmail.com>
2020-05-15 00:20:23 -0600

    :lipstick: Add ant design elements for main nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,33 +10,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { IconsProviderModule } from './icons-provider.module';
-import { NzLayoutModule } from 'ng-zorro-antd/layout';
-import { NzMenuModule } from 'ng-zorro-antd/menu';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { NZ_I18N } from 'ng-zorro-antd/i18n';
-import { es_ES } from 'ng-zorro-antd/i18n';
-import { registerLocaleData } from '@angular/common';
-import es from '@angular/common/locales/es';
 
-registerLocaleData(es);
+import { AntDesignModule } from './ant-design/ant-design.module';
+import { MainNavComponent } from './main-nav/main-nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainNavComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    IconsProviderModule,
-    NzLayoutModule,
-    NzMenuModule,
-    FormsModule,
-    HttpClientModule
+    AntDesignModule   
   ],
-  providers: [{ provide: NZ_I18N, useValue: es_ES }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
