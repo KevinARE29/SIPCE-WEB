745eb540e100eb9b25e80b8e5a5d99f254ebc7ed
Esmeralda López <lorenaeguadron@gmail.com>
2020-06-04 22:48:21 -0600

    :wrench: Change  prod environment api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiURL: "https://sipce-api.herokuapp.com/api/v1/"
+  // apiURL: "https://sipce-api.herokuapp.com/api/v1/"
+  apiURL: "https://432bddcece60.ngrok.io/api/v1/"
 };
