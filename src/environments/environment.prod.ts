7b3439bda95e4647e8315b8bec23df6e8cdbc5a3
Esmeralda López <lorenaeguadron@gmail.com>
2020-06-04 23:32:13 -0600

    :wrench: Reset prod environment api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  // apiURL: "https://sipce-api.herokuapp.com/api/v1/"
-  apiURL: "https://432bddcece60.ngrok.io/api/v1/"
+  apiURL: "https://sipce-api.herokuapp.com/api/v1/"
 };
