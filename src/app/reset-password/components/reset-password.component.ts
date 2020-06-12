1b3dbe9f2e615bef4e09489ffc18fd1d0ca19784
veronica <veronica25reyes@gmail.com>
2020-06-12 12:12:07 +0200

    :tada: Add reset password view

--- src/app/reset-password/components/reset-password.component.ts
+++ src/app/reset-password/components/reset-password.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ResetPasswordService } from '../shared/reset-password.service';
 
 @Component({
   selector: 'app-reset-password',
@@ -6,10 +9,85 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reset-password.component.css']
 })
 export class ResetPasswordComponent implements OnInit {
-
-  constructor() { }
+  resetPwd: FormGroup;
+  show: boolean = true;
+  message: boolean = false;
 
   ngOnInit(): void {
+    this.resetPwd = this.fb.group({
+      password: ['', [Validators.required]],
+      confirm: ['', [this.confirmValidator]],
+    });
   }
 
+  submitForm(value: { password: string; confirm: string }): void {
+    for (const key in this.resetPwd.controls) {
+      this.resetPwd.controls[key].markAsDirty();
+      this.resetPwd.controls[key].updateValueAndValidity();
+    }
+    console.log(value);
+  }
+
+  resetForm(e: MouseEvent): void {
+    e.preventDefault();
+    this.resetPwd.reset();
+    for (const key in this.resetPwd.controls) {
+      this.resetPwd.controls[key].markAsPristine();
+      this.resetPwd.controls[key].updateValueAndValidity();
+    }
+  }
+
+  validateConfirmPassword(): void {
+    setTimeout(() => this.resetPwd.controls.confirm.updateValueAndValidity());
+  }
+/*
+  userNameAsyncValidator = (control: FormControl) =>
+    new Observable((observer: Observer<ValidationErrors | null>) => {
+      setTimeout(() => {
+        if (control.value === 'JasonWood') {
+          // you have to return `{error: true}` to mark it as an error event
+          observer.next({ error: true, duplicated: true });
+        } else {
+          observer.next(null);
+        }
+        observer.complete();
+      }, 1000);
+    });
+*/
+    confirmValidator = (control: FormControl): { [s: string]: boolean } => {
+    if (!control.value) {
+        return { error: true, required: true };
+      } else if (control.value !== this.resetPwd.controls.password.value) {
+        return { confirm: true, error: true };
+      }
+      return {};
+    };
+
+  constructor(private fb: FormBuilder,
+    private resetPasswordService: ResetPasswordService,
+    private router: Router) { }
+
+
+
+  sendEmail() {
+    // validating the form 
+    for (const i in this.resetPwd.controls) {
+      this.resetPwd.controls[i].markAsDirty();
+      this.resetPwd.controls[i].updateValueAndValidity();
+    }
+
+    if (this.resetPwd.valid) {
+     // console.log(this.resetPassword.value)
+     // this.resetPasswordService.forgotPassword(this.resetPassword.value).subscribe(
+       // (response) => {
+         // this.show = false;
+        // },
+        // (error) => {
+         // console.log('entro al error');
+        // }
+     // );
+    }
+  }
+
+  
 }
--- src/app/reset-password/reset-password.module.ts
+++ src/app/reset-password/reset-password.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule }    from '@angular/common/http';
-
 import { AntDesignModule } from '../ant-design/ant-design.module';
 // routing and components
 import { ResetPasswordComponent } from './components/reset-password.component';
 import { ResetPasswordRoutingModule } from './reset-password-routing.module';
+// servicios
+import { ResetPasswordService } from './shared/reset-password.service';
 
 @NgModule({
   declarations: [ResetPasswordComponent],
@@ -14,7 +15,12 @@ import { ResetPasswordRoutingModule } from './reset-password-routing.module';
     CommonModule,
     HttpClientModule,
     AntDesignModule,
+    FormsModule, 
+    ReactiveFormsModule,
     ResetPasswordRoutingModule
-  ]
+  ],
+   providers: [
+    ResetPasswordService
+  ],
 })
 export class ResetPasswordModule { }
