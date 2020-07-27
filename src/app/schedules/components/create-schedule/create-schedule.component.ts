23ae41aadfac1be58152d49c828d2f02a07b3344
veronica <veronica25reyes@gmail.com>
2020-07-27 03:52:10 -0600

    :sparkles: List schedules

---
+++ src/app/schedules/components/create-schedule/create-schedule.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-schedule',
+  templateUrl: './create-schedule.component.html',
+  styleUrls: ['./create-schedule.component.css']
+})
+export class CreateScheduleComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
