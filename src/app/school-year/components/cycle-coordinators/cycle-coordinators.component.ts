e8693921726e71989711297fd0acf383f8acda7c
Esmeralda López <lorenaeguadron@gmail.com>
2020-08-19 21:44:45 -0600

    :sparkles: Get users by role

--- src/app/school-year/components/cycle-coordinators/cycle-coordinators.component.ts
+++ src/app/school-year/components/cycle-coordinators/cycle-coordinators.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/users/shared/user.service';
+import { User } from 'src/app/users/shared/user.model';
 
 @Component({
   selector: 'app-cycle-coordinators',
@@ -6,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cycle-coordinators.component.css']
 })
 export class CycleCoordinatorsComponent implements OnInit {
+  coordinators: User[];
 
-  constructor() { }
+  constructor(private userService: UserService) {}
 
   ngOnInit(): void {
+    this.getCycleCoordinators();
   }
 
+  getCycleCoordinators(): void {
+    this.userService.getUsersByRole(5).subscribe((data) => {
+      console.log(data);
+    });
+  }
 }
