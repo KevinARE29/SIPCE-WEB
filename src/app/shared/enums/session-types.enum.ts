199598d34ff82403b6ba3f68eb0dbabf67e170b3
Esmeralda López <lorenaeguadron@gmail.com>
2020-11-21 21:29:46 -0600

    :wrench: Add reports enum

--- src/app/shared/enums/session-types.enum.ts
+++ src/app/shared/enums/session-types.enum.ts
@@ -1,5 +1,5 @@
 export enum SessionTypes {
-    SESION = 'Sesión individual',
-    ENTREVISTA_DOCENTE = 'Entrevista con docente',
-    ENTREVISTA_PADRES = 'Entrevista con padres de familia'
+  SESION = 'Sesión individual',
+  ENTREVISTA_DOCENTE = 'Entrevista con docente',
+  ENTREVISTA_PADRES = 'Entrevista con padres de familia'
 }
