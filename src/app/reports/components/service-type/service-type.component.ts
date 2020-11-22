6ef0df5bcf5dff7f7f27ce435329ca2cd494c6be
Esmeralda López <lorenaeguadron@gmail.com>
2020-11-22 16:50:40 -0600

    :sparkles: Report by service type

---
+++ src/app/reports/components/service-type/service-type.component.ts
@@ -0,0 +1,142 @@
+import { Component, OnInit } from '@angular/core';
+
+import * as XLSX from 'xlsx';
+import { format, subMonths, differenceInCalendarDays } from 'date-fns';
+import { NzTableQueryParams } from 'ng-zorro-antd/table';
+import { NzNotificationService } from 'ng-zorro-antd/notification';
+
+import { ServiceTypes } from './../../../shared/enums/service-types.enum';
+
+import { ShiftPeriodGrade } from 'src/app/academic-catalogs/shared/shiftPeriodGrade.model';
+import { User } from 'src/app/users/shared/user.model';
+import { ReportService } from '../../shared/report.service';
+
+@Component({
+  selector: 'app-service-type',
+  templateUrl: './service-type.component.html',
+  styleUrls: ['./service-type.component.css']
+})
+export class ServiceTypeComponent implements OnInit {
+  loading = false;
+  listOfDisplayData: unknown[];
+  schoolYear: unknown;
+
+  // Search
+  searchParams: {
+    sessionType: string;
+    shiftId: number;
+    cycleId: number;
+    gradeId: number;
+    counselorId: number;
+    dateRange: unknown;
+  };
+
+  // Search lists
+  shifts: ShiftPeriodGrade[];
+  cycles: ShiftPeriodGrade[];
+  grades: ShiftPeriodGrade[];
+  counselors: User[];
+  serviceTypesList: string[];
+
+  constructor(private reportService: ReportService, private notification: NzNotificationService) {}
+
+  ngOnInit(): void {
+    this.init();
+    this.getCatalogsData();
+  }
+
+  init(): void {
+    const currentDate = new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate(), 23, 59, 59);
+    let date = subMonths(currentDate, 1);
+
+    this.searchParams = {
+      sessionType: null,
+      shiftId: null,
+      cycleId: null,
+      gradeId: null,
+      counselorId: null,
+      dateRange: null
+    };
+
+    this.shifts = new Array<ShiftPeriodGrade>();
+    this.cycles = new Array<ShiftPeriodGrade>();
+    this.grades = new Array<ShiftPeriodGrade>();
+    this.counselors = new Array<User>();
+
+    this.serviceTypesList = Object.keys(ServiceTypes).filter((k) => isNaN(Number(k)));
+    date = new Date(date.getFullYear(), date.getMonth(), date.getDate(), 0, 0, 0);
+
+    this.searchParams.dateRange = [date, currentDate];
+  }
+
+  getCatalogsData(): void {
+    this.reportService.mergeUserData().subscribe((data) => {
+      this.schoolYear = data['assignation'][0];
+      this.counselors = data['counselors']['data'];
+
+      this.schoolYear['shifts'].forEach((shift) => {
+        this.shifts.push({ id: shift.shift.id, name: shift.shift.name, active: shift.shift.active });
+      });
+    });
+  }
+
+  getSessionsReport(params: NzTableQueryParams): void {
+    this.loading = true;
+
+    this.reportService.getSessionsReport(params, this.searchParams, 'service_type').subscribe((data) => {
+      this.listOfDisplayData = [];
+      this.listOfDisplayData = data['data'];
+
+      this.loading = false;
+    });
+  }
+
+  cleanSelectors(type: string, id: number): void {
+    switch (type) {
+      case 'shift':
+        this.searchParams.cycleId = null;
+
+        this.cycles = id
+          ? this.schoolYear['shifts'].find((x) => x['shift'].id === id)['shift']['cycles']
+          : new Array<ShiftPeriodGrade>();
+        break;
+      case 'cycle':
+        const cycles = this.schoolYear['shifts'].find((x) => x['shift'].id === id)['shift']['cycles'];
+
+        this.grades = id ? cycles.find((x) => x['cycle'].id === id)['gradeDetails'] : new Array<ShiftPeriodGrade>();
+        if (this.grades) this.grades.sort((a, b) => a.id - b.id);
+        break;
+    }
+
+    this.searchParams.gradeId = null;
+
+    this.getSessionsReport(null);
+  }
+
+  onChangeDatePicker(result: Date[]): void {
+    this.searchParams.dateRange[0] = result[0];
+    this.searchParams.dateRange[1] = result[1];
+  }
+
+  disabledDate = (current: Date): boolean => {
+    // Can not select days after today
+    return differenceInCalendarDays(current, new Date()) > 0;
+  };
+
+  exportExcel(): void {
+    this.notification.create('info', 'Exportación en progreso', 'El archivo estará listo en unos momentos.');
+
+    const fileName = `Citas por tipo de sesión - ${format(new Date(), 'ddMMyyyyHHmmss')}.xlsx`;
+
+    /* Table id is passed over here */
+    const element = document.getElementById('excel-table');
+    const ws: XLSX.WorkSheet = XLSX.utils.table_to_sheet(element);
+
+    /* Generate workbook and add the worksheet */
+    const wb: XLSX.WorkBook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, 'Citas por tipo de sesión');
+
+    /* Save to file */
+    XLSX.writeFile(wb, fileName);
+  }
+}
--- src/app/reports/reports-routing.module.ts
+++ src/app/reports/reports-routing.module.ts
@@ -3,12 +3,19 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { AuthGuard } from '../login/guards/auth.guard';
 
+import { ServiceTypeComponent } from './components/service-type/service-type.component';
 import { SessionTypeComponent } from './components/session-type/session-type.component';
 
 const routes: Routes = [
   {
     path: 'tipos-de-sesion',
     component: SessionTypeComponent,
+    canActivate: [AuthGuard],
+    data: { permission: 7 } // TODO: Update
+  },
+  {
+    path: 'tipos-de-servicio',
+    component: ServiceTypeComponent,
     canActivate: [AuthGuard],
     data: { permission: 7 } // TODO: Update
   }
