4a7b0e5f6a6adfae00f4bc772d1968055cc947c9
Esmeralda López <lorenaeguadron@gmail.com>
2020-11-25 22:05:11 -0600

    ✨ Add editable final conclussion

--- src/app/expedients/components/expedient/expedient-view/expedient-view.component.ts
+++ src/app/expedients/components/expedient/expedient-view/expedient-view.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnChanges } from '@angular/core';
 
 // Models and services.
 import { Expedient } from 'src/app/expedients/shared/expedient.model';
@@ -9,14 +9,44 @@ import { ExpedientService } from 'src/app/expedients/shared/expedient.service';
   templateUrl: './expedient-view.component.html',
   styleUrls: ['./expedient-view.component.css']
 })
-export class ExpedientViewComponent {
+export class ExpedientViewComponent implements OnChanges {
   @Input() expedient: Expedient;
   @Input() isEditable: boolean;
+  @Input() studentId: number;
   @Output() initEditing = new EventEmitter<void>();
+
+  // Edit final comment.
+  editing = false;
+  loadingAction = false;
+  finalConclusion: string;
 
   constructor(private expedientService: ExpedientService) {}
 
+  ngOnChanges(): void {
+    this.cancelEditignFinalConclusion();
+  }
+
   onInitEditing(): void {
     this.initEditing.emit();
+  }
+
+  onInitiEditingFinalConclusion(): void {
+    this.editing = true;
+    this.finalConclusion = this.expedient.finalConclusion;
+  }
+
+  cancelEditignFinalConclusion(): void {
+    this.editing = false;
+    this.finalConclusion = '';
+  }
+
+  saveExpedient(): void {
+    this.loadingAction = true;
+    this.expedient.finalConclusion = this.finalConclusion;
+
+    this.expedientService.saveExpedient(this.studentId, this.expedient).subscribe(() => {
+      this.loadingAction = false;
+      this.cancelEditignFinalConclusion();
+    });
   }
 }
