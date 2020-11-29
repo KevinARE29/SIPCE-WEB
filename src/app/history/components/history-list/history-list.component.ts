c73894eede17ce7c4f37eac749b9093572781a85
Esmeralda López <lorenaeguadron@gmail.com>
2020-11-29 17:08:07 -0600

    ✨ Add filter list to history

--- src/app/history/components/history-list/history-list.component.ts
+++ src/app/history/components/history-list/history-list.component.ts
@@ -3,11 +3,12 @@ import { Component, OnInit } from '@angular/core';
 import { NzNotificationService } from 'ng-zorro-antd/notification';
 import { NzTableQueryParams } from 'ng-zorro-antd/table';
 
-import { ShiftPeriodGrade } from 'src/app/academic-catalogs/shared/shiftPeriodGrade.model';
-import { Pagination } from 'src/app/shared/pagination.model';
 import { UserService } from 'src/app/users/shared/user.service';
+import { SchoolYearService } from 'src/app/school-year/shared/school-year.service';
 import { HistoryService } from '../../shared/history.service';
 import { StudentWithHistory } from '../../shared/student-with-history.model';
+import { ShiftPeriodGrade } from 'src/app/academic-catalogs/shared/shiftPeriodGrade.model';
+import { Pagination } from 'src/app/shared/pagination.model';
 
 class ShiftGradeSection {
   id: number;
@@ -36,6 +37,7 @@ export class HistoryListComponent implements OnInit {
 
   constructor(
     private historyService: HistoryService,
+    private schoolYearService: SchoolYearService,
     private notification: NzNotificationService,
     private userService: UserService
   ) {}
@@ -61,17 +63,32 @@ export class HistoryListComponent implements OnInit {
     this.loadingSearchLists = true;
 
     this.userService.getUserProfile().subscribe((data) => {
-      // If roles include "Docente" or "Docente Auxiliar", show only the sections assigned.
+      // If roles include one of those, show all shifts.
       if (
         (data['roles'] as unknown[]).some((role) => {
-          return ['Docente', 'Docente Auxiliar'].includes(role['name']);
+          return ['Director', 'Coordinador de Ciclo', 'Orientador'].includes(role['name']);
         })
       ) {
+        this.getAllShifts();
+
+        // Else, show only the sections assigned to teachers and auxiliary teachers.
+      } else {
+        // Set the filters for teachers.
         if (data['teacherAssignation']) {
           (data['teacherAssignation'] as unknown[]).forEach((assigment) => {
-            const shift: ShiftGradeSection = assigment['shift'];
-            const grade: ShiftGradeSection = assigment['grade'];
-            const section: ShiftGradeSection = assigment['section'];
+            const shift: ShiftGradeSection = {
+              id: assigment['shift'].id,
+              name: assigment['shift'].name
+            };
+
+            const grade: ShiftGradeSection = {
+              id: assigment['grade'].id,
+              name: assigment['grade'].name
+            };
+            const section: ShiftGradeSection = {
+              id: assigment['section'].id,
+              name: assigment['section'].name
+            };
 
             grade.children = [section];
             shift.children = [grade];
@@ -80,8 +97,90 @@ export class HistoryListComponent implements OnInit {
           });
         }
 
+        // Set the filters for auxiliary teachers.
+        if (data['auxTeacherAssignation']) {
+          Object.values(data['auxTeacherAssignation']).forEach((assignation: unknown[]) => {
+            assignation.forEach((assigment) => {
+              // For each shift, check if it's already registered.
+              let shift = this.shifts.find((value) => value.id === assigment['shift'].id);
+
+              if (!shift) {
+                shift = {
+                  id: assigment['shift'].id,
+                  name: assigment['shift'].name,
+                  children: []
+                };
+
+                this.shifts.push(shift);
+              }
+
+              // For each grade on the shift, check if it's already registered.
+              assigment['gradeDetails'].forEach((gradeDetail) => {
+                let grade = shift.children.find((value) => value.id === gradeDetail['grade'].id);
+
+                if (!grade) {
+                  grade = {
+                    id: gradeDetail['grade'].id,
+                    name: gradeDetail['grade'].name,
+                    children: []
+                  };
+
+                  shift.children.push(grade);
+                }
+
+                // For each section on the grade, check if it's already registered.
+                gradeDetail['sectionDetails'].forEach((sectionDetail) => {
+                  let section = grade.children.find((value) => value.id === sectionDetail['section'].id);
+
+                  if (!section) {
+                    section = {
+                      id: sectionDetail['section'].id,
+                      name: sectionDetail['section'].name
+                    };
+
+                    grade.children.push(section);
+                  }
+                });
+              });
+            });
+          });
+        }
+
         this.loadingSearchLists = false;
       }
+    });
+  }
+
+  // List of all shifts, grades and sections of the current year.
+  getAllShifts(): void {
+    this.schoolYearService.getSchoolYear().subscribe((schoolYear) => {
+      const current = schoolYear.find((value) => value.status === 'En curso');
+
+      if (current && current.shifts) {
+        this.shifts = current.shifts.map((shiftDetail) => {
+          return {
+            id: shiftDetail['shift'].id,
+            name: shiftDetail['shift'].name,
+            children: shiftDetail['shift'].cycles.reduce((arr, cycle) => {
+              cycle['gradeDetails'].forEach((gradeDetail) => {
+                arr.push({
+                  id: gradeDetail['grade'].id,
+                  name: gradeDetail['grade'].name,
+                  children: gradeDetail['sectionDetails'].map((sectionDetail) => {
+                    return {
+                      id: sectionDetail['section'].id,
+                      name: sectionDetail['section'].name
+                    };
+                  })
+                });
+              });
+
+              return arr;
+            }, [])
+          };
+        });
+      }
+      this.loadingSearchLists = false;
     });
   }
 
