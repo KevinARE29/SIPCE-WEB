cf058ee06320e3247220718d13f3d5221c9768ad
Esmeralda López <lorenaeguadron@gmail.com>
2020-12-05 00:04:55 -0600

    ✏️ Fix typo

--- src/app/expedients/shared/session.model.ts
+++ src/app/expedients/shared/session.model.ts
@@ -33,7 +33,7 @@ export class Session {
   // Individual session request
   interventionProgramId: number;
 
-  // Teach interview request
+  // Teacher interview request
   participants: number[];
 
   // Teacher interview response
