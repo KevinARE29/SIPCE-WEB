62601cc9fac26ef497fdf7d6e929157d29029f98
Esmeralda López <lorenaeguadron@gmail.com>
2020-12-08 01:28:45 -0600

    :construction: Display shifts & grades percentages

--- src/app/school-year/components/school-year-end-summary/school-year-end-summary.component.ts
+++ src/app/school-year/components/school-year-end-summary/school-year-end-summary.component.ts
@@ -1,12 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 import { SchoolYearService } from '../../shared/school-year.service';
 
+interface Section {
+  id: number;
+  name: string;
+  teacher: string;
+  closed: boolean;
+}
+
+interface Grade {
+  id: number;
+  name: string;
+  expand: boolean;
+  gradePercentage: number;
+  sections: Section[];
+}
+
+interface Shift {
+  id: number;
+  name: string;
+  shiftPercentage: number;
+  grades: Grade[];
+}
+
 @Component({
   selector: 'app-school-year-end-summary',
   templateUrl: './school-year-end-summary.component.html',
   styleUrls: ['./school-year-end-summary.component.css']
 })
 export class SchoolYearEndSummaryComponent implements OnInit {
+  data: unknown;
+  shifts: Shift[] = [];
+  expandSet = new Set<number>();
+
   constructor(private schoolYearService: SchoolYearService) {}
 
   ngOnInit(): void {
@@ -16,7 +42,17 @@ export class SchoolYearEndSummaryComponent implements OnInit {
 
   getData(): void {
     this.schoolYearService.getClosingStatus().subscribe((data) => {
-      console.log(data);
+      this.data = data;
+      this.shifts = data['shifts'];
+      console.log(this.data, this.shifts);
     });
+  }
+
+  onExpandChange(id: number, checked: boolean): void {
+    if (checked) {
+      this.expandSet.add(id);
+    } else {
+      this.expandSet.delete(id);
+    }
   }
 }
