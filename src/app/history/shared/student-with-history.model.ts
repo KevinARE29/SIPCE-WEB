dcca01e3635beaef64db464453a46e7a72820aab
Esmeralda López <lorenaeguadron@gmail.com>
2020-12-14 23:19:26 -0600

    ✨ Add new signature to behavioral history report

--- src/app/history/shared/student-with-history.model.ts
+++ src/app/history/shared/student-with-history.model.ts
@@ -1,8 +1,10 @@
 import { Student } from 'src/app/students/shared/student.model';
+import { User } from 'src/app/users/shared/user.model';
 import { History } from './history.model';
 
 export class StudentWithHistory {
   student: Student;
   behavioralHistory: History;
   date: string;
+  user: User;
 }
