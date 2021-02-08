136423c9f6e5deba685b5d38e019b8eab8f29815
Esmeralda López <lorenaeguadron@gmail.com>
2021-02-08 09:23:14 -0600

    :wrench: Reset prod environment api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiURL: 'https://dev-sipce-api.herokuapp.com/api/v1/',
-  socketURL: 'https://dev-sipce-api.herokuapp.com',
+  apiURL: 'https://sipce-api.herokuapp.com/api/v1/',
+  socketURL: 'https://sipce-api.herokuapp.com',
   whiteLogo: 'assets/logos/logo-blanco.png',
   logo: 'assets/logos/logo-color.png',
   name: 'Liceo Salvadoreño'
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  apiURL: 'https://dev-sipce-api.herokuapp.com/api/v1/',
-  socketURL: 'https://dev-sipce-api.herokuapp.com',
+  apiURL: 'https://sipce-api.herokuapp.com/api/v1/',
+  socketURL: 'https://sipce-api.herokuapp.com',
   whiteLogo: 'assets/logos/logo-blanco.png',
   logo: 'assets/logos/logo-color.png',
   name: 'Liceo Salvadoreño'
